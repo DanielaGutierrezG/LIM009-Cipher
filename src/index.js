6e734b8ceed8ea6824c78b30b35791dfe7f03997
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-02-25 10:06:26 -0500

    Codigo cifrado en JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-/* Acá va tu código */
+let palabra='';
+let desplazamiento = 2;
+let nuevoNumero;
+let nuevaPalabra;
+let ascii;
+let numFormula;
+let asciiNumero;
+
+for(let i = 0; i < palabra.length; i++){
+ ascii = palabra[i].charCodeAt();
+ if (65 <= ascii <= 90) {
+ (numFormula = (ascii - 65 + desplazamiento) % 26 + 65);
+ }
+ console.log(nuevaPalabra = String.fromCharCode(numFormula));
+};

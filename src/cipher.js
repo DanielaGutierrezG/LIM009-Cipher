11a16e6ab321b53818ce93ecf0b914811fc70a68
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-03-11 08:51:51 -0500

    Añadiendo gh-pages

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,39 @@
 window.cipher = {
-  encode: (textoDescifrado,desplazamiento) => {
-   let nuevoResultadoCifrado;
-   let asciiDescifrado;
-   let resultadoCifrado = "";
+  encode: (textoCifrar,desplazamiento) => {
+   let asciiTextoCifrar;
+   let textoCifrado ="";
 
 
-    for(let i = 0 ; i < textoDescifrado.length ; i++){
-      asciiDescifrado =textoDescifrado[i].charCodeAt();
-     //if( 65 <= asciiDescifrado <= 90 ){
-      nuevoResultadoCifrado = String.fromCharCode((asciiDescifrado - 65 + desplazamiento) % 26 + 65);
-     resultadoCifrado += nuevoResultadoCifrado;
-     // }
+    for(let i = 0 ; i < textoCifrar.length ; i++){
+
+      asciiTextoCifrar =textoCifrar[i].charCodeAt();
+
+     if( 65 <= asciiTextoCifrar <= 90 ){
+      textoCifrado += String.fromCharCode((asciiTextoCifrar - 65 + desplazamiento) % 26 + 65);
+    }else if( 97 <= asciiTextoCifrar <= 122 ){
+    textoCifrado += String.fromCharCode((asciiTextoCifrar - 97 + desplazamiento) % 26 + 97);
     }
-    return resultadoCifrado;
-  },
-
-  decode: (textoCifrado,desplazamiento) => {
-    let nuevoNumero;
-    let nuevoResultadoDescifrado;
-    let nuevoNumeroCifrado;
-    let asciiCifrado;
-    let resultadoDescifrado=" ";
-
-   for(let i = 0 ; i < textoCifrado.length ; i++){
-     asciiCifrado=textoCifrado[i].charCodeAt();
-      // if( 65 <= asciiCifrado<=90){
-         nuevoNumero=(asciiCifrado-65-desplazamiento);
-       // }  
-       while (nuevoNumero < 0){
-          nuevoNumero=nuevoNumero + 26;
-          }
-          nuevoNumeroCifrado=nuevoNumero%26+65
-          nuevoResultadoDescifrado=String.fromCharCode(nuevoNumeroCifrado)
-          resultadoDescifrado += nuevoResultadoDescifrado;
-     }
-    return resultadoDescifrado;
   }
+
+  return textoCifrado;
+
+},
+
+
+  decode: (textoDescifrar,desplazamiento) => {
+    let asciiTextoDescifrar;
+    let textoDescifrado="";
+
+    for(let i = 0 ; i < textoDescifrar.length ; i++){
+
+      asciiTextoDescifrar =textoDescifrar[i].charCodeAt();
+
+     if( 65 <= asciiTextoDescifrar <= 90 ){
+      textoDescifrado += String.fromCharCode((asciiTextoDescifrar + 65 - desplazamiento) % 26 + 65);
+      }else if( 97 <= asciiTextoDescifrar <= 122 ){
+      textoDescifrado += String.fromCharCode((asciiTextoDescifrar + 33 - desplazamiento) % 26 + 97);
+      }
 }
+    return textoDescifrado;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+
+ const pagina1 = document.getElementById("pagina1");
+ const pagina2 =document.getElementById("pagina2");
+
 const btn=document.getElementById('btn');
 btn.addEventListener("click",()=>{
 
@@ -5,60 +9,24 @@ btn.addEventListener("click",()=>{
   pagina2.style.display="block";
 })
 
-let textoDescifrado;
 let desplazamiento;
-let textoCifrado;
+let textoCifrado ;
+let textoDescifrar;
+let textoDescifrado;
+let textoCifrar;
+
 
 document.getElementById('btncifrar').addEventListener("click",()=>{
-  textoDescifrado=document.getElementById('descifrado').value;
+  textoCifrar=document.getElementById('descifrado').value;
   desplazamiento=parseInt(document.getElementById('number').value);
-  resultadoCifrado=cipher.encode(textoDescifrado,desplazamiento);
-  document.getElementById('cifrado').value= resultadoCifrado;
+  textoCifrado=cipher.encode(textoCifrar,desplazamiento);
+  document.getElementById('cifrado').value= textoCifrado;
 })
 
 document.getElementById('btndescifrar').addEventListener("click",()=>{
-  textoCifrado=document.getElementById('cifrado').value;
+  textoDescifrar=document.getElementById('cifrado').value;
   desplazamiento=parseInt(document.getElementById('number').value);
-  resultadoDescifrado=cipher.decode(textoCifrado,desplazamiento);
-  document.getElementById('descifrado').value=resultadoDescifrado;
+  textoDescifrado=cipher.decode(textoDescifrar,desplazamiento);
+  document.getElementById('descifrado').value=textoDescifrado;
 
 })
-
-window.cipher = {
-  encode: (textoDescifrado,desplazamiento) => {
-   let ascii;
-   let nuevoResultCifrado;
-   let resultadoCifrado=" ";
-
-    for(let i = 0 ; i < textoDescifrado.length ; i++){
-      asciiDescifrado =textoDescifrado[i].charCodeAt();
-     if( 65 <= asciiDescifrado <= 90 ){
-      nuevoResultadoCifrado = String.fromCharCode((asciiDescifrado - 65 + desplazamiento) % 26 + 65);
-     console.log(resultadoCifrado += nuevoResultadoCifrado);
-      }
-    }
-    return resultadoCifrado;
-  },
-
-  decode: (textoCifrado,desplazamiento) => {
-    /* Acá va tu código */
-    let nuevoNumero;
-    let nuevoResultadoDescifrado;
-    let nuevoNumeroCifrado;
-    let asciiCifrado;
-    let resultadoDescifrado=" ";
-
-   for(let i = 0 ; i < textoCifrado.length ; i++){
-     asciiCifrado=textoCifrado[i].charCodeAt();
-       if( 65 <= asciiCifrado<=90){
-         nuevoNumero=(asciiCifrado-65-desplazamiento);
-        }  while (nuevoNumero < 0){
-          nuevoNumero=nuevoNumero + 26;
-          }
-          nuevoNumeroCifrado=nuevoNumero%26+65
-          console.log(nuevoResultadoDescifrado=String.fromCharCode(nuevoNumeroCifrado))
-          resultadoDescifrado += nuevoResultadoDescifrado;
-     }
-    return resultadoDescifrado;
-  }
-}
